58e988f30d4fd61356056075a872052583513b9c
ehasan-karbasian <ehsan.karbasian@gmail.com>
2023-01-23 17:24:22 +0330

    create mocked context functionalities

--- context.py
+++ context.py
@@ -7,6 +7,27 @@ class Context:
         self.locations = locations
         self.__graph = graph
     
+    def add_new_map_location(self):
+        pass
+    
+    def add_new_private_location(self):
+        pass
+    
+    def search_location_by_name(self):
+        pass
+    
     def navigate(self, source, destination):
         routes = self.__navigator.navigate(self.__graph, source, destination)
         return routes
+    
+    def find_the_nearest_location_by_type(self):
+        pass
+    
+    def get_location_info(self):
+        pass
+    
+    def make_location_favourite(self):
+        pass
+    
+    def rate_location(self):
+        pass
--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import json
+import copy
+
 from context import Context
 from src.graph import Graph
 from src.navigation import OnWalkNavigator, OnRideNavigator
 from src.user import User
 from src.location import MapLocation, PrivateLocation
+
 
 graph = Graph()
 graph.add_node(node_name='azadi_sq')
@@ -42,8 +45,9 @@ private_location_2 = PrivateLocation(7, 23, name='home', user=user_2)
 locations_1 = [map_location_1, map_location_2, map_location_3, map_location_4, map_location_5, private_location_1]
 locations_2 = [map_location_1, map_location_2, map_location_3, map_location_4, map_location_5, private_location_2]
 
-graph_1 = graph
-graph_2 = graph
+
+graph_1 = copy.deepcopy(graph)
+graph_2 = copy.deepcopy(graph)
 graph_1.add_node(private_location_1.name)
 graph_1.add_edge(private_location_1.name, 'tehran_university', edge_weight=6)
 graph_1.add_edge('resalat_sq', private_location_1.name, edge_weight=5)
@@ -54,7 +58,34 @@ graph_2.add_edge('tehran_university', private_location_1.name, edge_weight=1)
 context_1 = Context(navigator=onWalkNavigator, user=user_1, locations=locations_1, graph=graph_1)
 context_2 = Context(navigator=onRideNavigator, user=user_2, locations=locations_2, graph=graph_2)
 
+
+# Call context functionalities:
+
+# امکان تعریف مکان جدید در نقشه
+context_1.add_new_map_location()
+
+# امکان علامت گذاری مکانی در نقشه
+context_1.add_new_private_location()
+
+# سرچ کردن بدنبال یک مکان با اسم آن
+context_1.search_location_by_name()
+
+# مسیریابی از نقطه ای به نقطه ی دیگر بصورت مسیر پیاده رو
 routes_1 = context_1.navigate(map_location_1, map_location_4)
+print(routes_1[0]['distance'])
+
+# مسیریابی از نقطه ای به نقطه ی دیگر بصورت مسیر ماشین رو
 routes_2 = context_2.navigate(map_location_1, map_location_4)
 print(json.dumps(routes_2, sort_keys=True, indent=4))
-print(routes_1[0]['distance'])
+
+# پیدا کردن نزدیک ترین نوع از یک مکان
+context_1.find_the_nearest_location_by_type()
+
+# دیدن جزییات مکان توسط کاربر
+context_1.get_location_info()
+
+# فیوریت کردن یک مکان خاص روی نقشه
+context_1.make_location_favourite()
+
+# ریت دادن به یک مکان خاص از نقشه از طرف کاربر
+context_1.rate_location()
